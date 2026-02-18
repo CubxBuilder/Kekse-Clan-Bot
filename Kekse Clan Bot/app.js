f8cb51132390fb24c0210864f63f288d3ebac8a0
CubeBuilder <CubeBuilder@web.de>
2026-02-18 19:32:24 +0100

    Update app.js

--- Kekse Clan Bot/app.js
+++ Kekse Clan Bot/app.js
@@ -70,6 +70,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.GuildMembers
   ],
