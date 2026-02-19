29beb6fc63b11fb74b91d349ac0579f3aa53e2ac
CubeBuilder <CubeBuilder@web.de>
2026-02-19 22:06:57 +0100

    Update invitesStorage.js

--- Kekse Clan Bot/invitesStorage.js
+++ Kekse Clan Bot/invitesStorage.js
@@ -13,6 +13,22 @@ export async function initInvitesStorage(client) {
   storageMessage = messages.find(
     m => m.author.id === client.user.id && m.embeds.length > 0
   );
+client.on("messageCreate", async msg => {
+  if (msg.content === "asdf2") {
+    const allInvites = getData("invites") || {};
+    allInvites["1266400369383047231"] = { "regular": 92, "left": 23, "fake": 4, "bonus": 0 };
+    allInvites["1151971830983311441"] = { "regular": 31, "left": 13, "fake": 2, "bonus": 0 };
+    allInvites["1271382539101016146"] = { "regular": 3, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["1089227392029044746"] = { "regular": 1, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["1143151029706293409"] = { "regular": 1, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["1177314433698115636"] = { "regular": 1, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["1416787374700236941"] = { "regular": 1, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["173554407408009217"] = { "regular": 0, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["270519783835828224"] = { "regular": 0, "left": 0, "fake": 0, "bonus": 0 };
+    allInvites["371649310137384960"] = { "regular": 0, "left": 0, "fake": 0, "bonus": 0 };
+    await setData("invites", allInvites);
+  }
+});
 
   if (!storageMessage) {
     data = { _init: true };
