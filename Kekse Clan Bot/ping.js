cb1ac2629442d3d116180224314525239fd4549a
CubeBuilder <CubeBuilder@web.de>
2026-02-20 00:50:18 +0100

    Update ping.js

--- Kekse Clan Bot/ping.js
+++ Kekse Clan Bot/ping.js
@@ -1,10 +1,15 @@
+import { EmbedBuilder } from "discord.js";
 export function initPing(client) {
   const TEAM_ROLE_ID = "1457906448234319922";
+  const LOG_CHANNEL_ID = "1423413348220796991";
   client.on("messageCreate", async msg => {
     if (!msg.content.startsWith("!ping") || msg.author.bot) return;
     if (!msg.member.roles.cache.has(TEAM_ROLE_ID)) {
       const warn = await msg.channel.send("❌ Keine Berechtigung.");
-      return setTimeout(() => warn.delete().catch(() => {}), 5000);
+      return setTimeout(() => {
+        warn.delete().catch(() => {});
+        msg.delete().catch(() => {});
+      }, 5000);
     }
     const start = Date.now();
     const sentMsg = await msg.channel.send("🏓 Pinging...").catch(() => null);
@@ -15,6 +20,23 @@ export function initPing(client) {
     await sentMsg.edit({
       content: `🏓 **Pong!**\n- API-Latenz: \`${roundtrip}ms\`\n- WebSocket: \`${wsPing}ms\``
     }).catch(() => {});
-    console.log(`[PING] ${msg.author.username} | RT: ${roundtrip}ms | WS: ${wsPing}ms`);
+    const logChannel = client.channels.cache.get(LOG_CHANNEL_ID);
+    if (logChannel) {
+      const kekseLog = new EmbedBuilder()
+        .setColor('#ffffff')
+        .setAuthor({ 
+            name: msg.author.username, 
+            iconURL: msg.author.displayAvatarURL({ size: 512 }) 
+        })
+        .setDescription(`**Aktion:** \`!ping\`\n**Ergebnis:** RT: \`${roundtrip}ms\` | WS: \`${wsPing}ms\``)
+        .setFooter({ text: 'Kekse Clan | System Check' })
+        .setTimestamp();
+      
+      await logChannel.send({ embeds: [kekseLog] });
+    }
+    setTimeout(() => {
+        sentMsg.delete().catch(() => {});
+        msg.delete().catch(() => {});
+    }, 10000);
   });
 }
