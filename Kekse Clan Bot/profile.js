cd4bc2549338b07b164255bb1101372875dc7f16
CubeBuilder <CubeBuilder@web.de>
2026-02-20 18:39:55 +0100

    Update profile.js

--- Kekse Clan Bot/profile.js
+++ Kekse Clan Bot/profile.js
@@ -2,17 +2,24 @@ import cors from "cors";
 
 export async function initProfileStatus(client, app) {
   app.use(cors());
-  
+
   const MY_ID = "1151971830983311441";
   let currentStatus = "offline";
+  let currentActivity = "";
 
-  client.on("presenceUpdate", (old, newPresence) => {
-    if (newPresence?.userId === MY_ID) {
-      currentStatus = newPresence.status;
+  const updateData = (presence) => {
+    if (presence?.userId === MY_ID) {
+      currentStatus = presence.status;
+      currentActivity = presence.activities[0]?.name || "";
     }
-  });
+  };
+
+  client.on("presenceUpdate", (old, newPresence) => updateData(newPresence));
 
   app.get("/api/status", (req, res) => {
-    res.json({ status: currentStatus });
+    res.json({ 
+      status: currentStatus,
+      activity: currentActivity 
+    });
   });
 }
